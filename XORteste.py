d8e2ebbefe637113b21f4ad7a552605d65f2fa34
Vinicius Carvalho <vinisilvacar@gmail.com>
2017-05-22 06:52:09 -0800

    Add comments about libraries and variables

--- XORteste.py
+++ XORteste.py
@@ -1,15 +1,19 @@
+# Aprendizagem Supervisionada padrão - SupervisedDataSet
+#   Conjunto de Entradas que tenha entradas e alvos.
 from pybrain.datasets import SupervisedDataSet
+
+# Atalho BuildNetwork
+#   Essa chamada retorna uma rede que tem duas entradas, três oculto e um único
+#   neurônio de saída.
+#   Na camada oculta, é construída a função Sigmoide por padrão
 from pybrain.tools.shortcuts import buildNetwork
+
+# Para ajustar os parâmetros dos módulos na aprendizagem supervisionada usando
+# backpropagation.
 from pybrain.supervised import BackpropTrainer
 
-# Coisas a serem feitas:
-# Descobrir quais sao as variaveis importantes
-# Ligar elas ao conteudo da professora, pra quando ela perguntar e querer que algo seja mudado
-# docs do pyBrain: http://pybrain.org/docs/
-# deem uma pesquisada, se eu tiver feito algo errado, ou se tiver um jeito melhor, a vontade
 
-
-ds = SupervisedDataSet(2,1)
+ds = SupervisedDataSet(2, 1) # Suporta entradas bidimensionais e alvos dimensionais
 
 ds.addSample((0, 0), (0,))
 ds.addSample((0, 1), (1,))
@@ -19,8 +23,10 @@ ds.addSample((1, 1), (0,))
 for inpt, target in ds:
     print inpt, target
 
-
+# buildNetwork(nº entradas, nº intermediária, nº saídas, bias)
 net = buildNetwork(ds.indim, 4, ds.outdim, bias=True)
+
+# Os trainers tomam um módulo e um conjunto de dados para treinar o módulo para ajustar os dados no conjunto de dados.
                                                                     #Trocar essas linhas para:
 trainer = BackpropTrainer(net,ds, learningrate=0.01, momentum=0.99) # trainer = BackpropTrainer(net,ds)
 for epoch in range(0, 3000):                                        # for epoch in range(0, 10000):
